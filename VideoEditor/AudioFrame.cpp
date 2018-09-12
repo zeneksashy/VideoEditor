813215e13dcddb902eac60ea0e07817233efd675
Gracjan Zeniuk <gracjan.zeniuk@gmail.com>
2018-09-12 12:47:00 +0200

    AudioFrame class refactorisation

--- VideoEditor/AudioFrame.cpp
+++ VideoEditor/AudioFrame.cpp
@@ -7,8 +7,9 @@ AudioFrame::AudioFrame(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-	setMinimumHeight(50);
+	setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+	setMinimumHeight(60);
+	
 }
 
 AudioFrame::~AudioFrame()
@@ -23,34 +24,20 @@ void AudioFrame::Initialize(QString path)
 	connect(decoder.data(), SIGNAL(bufferReady()), this, SLOT(readBuffer()));
 	connect(decoder.data(), &QAudioDecoder::finished, this, &AudioFrame::audioDecoded);
 	decoder->start();
-	
-	tileLenght = 4096;
-	
-	//CreatePixmaps(rect().size());
 }
+
 void AudioFrame::readBuffer()
 {
 	auto tempBuff = decoder->read();
-	//if (isFirstTimeRead)
-//	{
-	//	
-	//	
-	//	windowSize = audioLength(format, windowsDuration);
-	//	//CalculateTiles();
-	//	isFirstTimeRead = false;
-//	}
-	this->format = tempBuff.format();
-	bufferbytecount = tempBuff.byteCount();
-	auto sample = this->format.sampleRate();
-	analyser.reset(recognizer.RrecognizeFrameType(tempBuff, format));
+	if (isFirstTimeRead)
+	{
+		this->format = tempBuff.format();
+		sampleLenght = format.sampleSize();
+		analyser.reset(recognizer.RrecognizeFrameType(tempBuff, format));
+	}
 	QAudioBuffer::S16U *frames = tempBuff.data<QAudioBuffer::S16U>();
 	for (size_t i = 0; i < tempBuff.frameCount(); i++)
 	{
-		auto wat = tempBuff.frameCount();
-		double x = frames[i].average();
-		double left = frames[i].left;
-		double right = frames[i].right;
-
 		double amplitude = (double)frames[i].average() / 32767;
 		double temp = 0;
 		if (amplitude < 0)
@@ -63,13 +50,10 @@ void AudioFrame::readBuffer()
 			temp = std::log10(amplitude);
 		}
 		double db = 20 *temp;
-		//db = 24*(db-1)
-		//db = qMax(qreal(0.0), db);
-		//db = qMin(qreal(1.0), db);
 		audioFrames.push_back(db);
 	}
-	buffers.push_back(tempBuff);
 }
+
 void AudioFrame::Initialize(AudioAnalyser *analyser, const QAudioFormat & format, qint64 audioBufferSize)
 {
 	this -> format = format;
@@ -78,161 +62,32 @@ void AudioFrame::Initialize(AudioAnalyser *analyser, const QAudioFormat & format
 	
 
 }
-long long AudioFrame::audioLength(const QAudioFormat &format, qint64 microSeconds)
-{
-	qint64 result = (format.sampleRate() * format.channelCount() * (format.sampleSize() / 8))
-		* microSeconds / 1000000;
-	result -= result % (format.channelCount() * format.sampleSize());
-	return result;
-}
-
-void AudioFrame::PaintTiles(long long lenght, QByteArray buffer)
-{
-	bufferLenght = lenght;
-	this->data = buffer;
-	for (size_t i = 0; i < pixmaps.size(); i++)
-	{
-		PaintTile(i);
-	}
-	update();
-	emit LineDrawn(this);
-	
-}
-int windowPixelOffset(qint64 positionOffset)
-{
-	const int result = (qreal(positionOffset) / 1500);
-	return result;
-}
 void AudioFrame::paintEvent(QPaintEvent*)
 {
-
 	QPainter painter(this);
-	painter.fillRect(rect(), Qt::red);
-	int destLeft = 0;
-	int destRight = 20;
-	int pos = 0;
+	painter.fillRect(rect(), Qt::gray);
 	QSize size(rect().size());
-	const int spectrWidth = 1000;
-	const int spectrHeight = 500;
 	QPixmap spectrPixmap(size);
 	QPainter p(&spectrPixmap);
 	QPen pen(Qt::blue);
 	painter.setPen(pen);
-	int i = 0;
-	//rect().setWidth(audioSamples.size());
-	for (int ir = 0; ir <audioSamples.size(); ir++)
+	setMinimumWidth(audioSamples.size());
+	for (int i = 0; i <audioSamples.size(); i++)
 	{
 		double plot = 0;
-			plot = audioSamples[ir]*25;
+			plot = audioSamples[i]*30;
 			QLine line(i, (size.height() / 2)+plot, i, (size.height() / 2) - plot);
-			// QLine line(i, size.height() / 2+plot, i, plot);
 			 painter.drawLine(line);
-			 //i+=linesize;
-			 ++i;
-		
-	
 	}
 }
-void AudioFrame::PaintTile(int index)
-{
-	//qint64 zmienic na T
-	const qint64 tileStart = index * tileLenght;
-	short previousPcmValue = 0;
-	
-	const qint16* base = reinterpret_cast<const qint16*>(data.constData());
-	const qint16* buffer = base + (tileStart / 2);
-	if (buffer > base)
-	{
-		previousPcmValue = *(buffer - format.channelCount());
-	}
-	const auto previousRealValue = analyser->ConvertInput(previousPcmValue);
-	const int originY = ((previousRealValue + 1.0) / 2) * pixmapSize.height();
-	const QPoint origin(0, originY);
-	QPainter painter(pixmaps[index]);
-	painter.fillRect(pixmaps[index]->rect(), Qt::blue);
-	QPen pen(Qt::green);
-	painter.setPen(pen);
-	const int numSamples = tileLenght / (2 * format.channelCount());
-	QLine line(origin, origin);
-	int x = 0;
-	for (size_t i = 0; i < numSamples; i++)
-	{
-		const qint16* ptr = buffer + i * format.channelCount();
-		//const int offset = reinterpret_cast<const char*>(ptr) - this->data.constData();
-		const qint16 pcmValue = *ptr;
-		const qreal realValue = analyser->ConvertInput(pcmValue);
-		// int x = tilePixelOffset(i * 2 * format.channelCount());
-		x += 5;
 
-		 int y = ((realValue + 1.0) / 2) * pixmapSize.height();
-	//	 y +=x;
-	
-		line.setP2(QPoint(x, y));
-		painter.drawLine(line);
-		line.setP1(line.p2());
-	}
-
-}
-void AudioFrame::smth(int index)
-{
-	
-
-}
 void AudioFrame::audioDecoded()
 {
-	singlebufferLenght = PowerOfTwo<12>::Result *(format.sampleSize() / 8)*format.channelCount();
-	for each (auto buffer in buffers)
-	{
-		data.append(QByteArray::fromRawData(buffer.constData<char>(), singlebufferLenght));
-	}
 	rms();
 	nomralize();
-	//tileLenght = data.size();
-	smth(0);
 	update();
-	//PaintTiles(data.size(), data);
 	emit LineDrawn(this);
 }
-
-void AudioFrame::ReadFormat()
-{
-	//this->decoder
-}
-
-int AudioFrame::tilePixelOffset(qint64 positionOffset) const
-{
-	const int result = (qreal(positionOffset) / tileLenght) * pixmapSize.width();
-	return result;
-}
-
-void AudioFrame::CalculateTiles()
-{
-	int tilesCount;
-	if (tileLenght > windowSize)
-		tilesCount = 2;
-	else
-	{
-		tilesCount = windowSize / tileLenght + 1;
-		if (windowSize % tileLenght)
-			++tilesCount;
-	}
-	pixmaps.assign(tilesCount, 0);
-	CreatePixmaps(rect().size());
-}
-
-void AudioFrame::CreatePixmaps(QSize widgetSize)
-{
-	pixmapSize = widgetSize;
-	pixmapSize.setWidth(qreal(widgetSize.width()) * tileLenght / windowSize);
-	for (size_t i = 0; i < pixmaps.size(); i++)
-	{
-		delete pixmaps[i];
-		pixmaps[i] = 0;
-		pixmaps[i] = new QPixmap(pixmapSize);
-	}
-
-}
-
 void AudioFrame::nomralize()
 {
 	auto vmax = std::max_element(audioSamples.begin(), audioSamples.end());
@@ -249,7 +104,7 @@ void AudioFrame::nomralize()
 		audioSamples[i] /= (max - min);
 	}
 }
-// delete 4096 and add other number
+// delete 4096 and add variable number
 void AudioFrame::rms()
 {
 	auto size = audioFrames.size()/4096;
@@ -266,5 +121,4 @@ void AudioFrame::rms()
 
 		k += 4096;
 	}
-	
 }
--- VideoEditor/AudioFrame.h
+++ VideoEditor/AudioFrame.h
@@ -17,40 +17,21 @@ public:
 	~AudioFrame();
 	void Initialize(QString);
 	void Initialize(AudioAnalyser*,const QAudioFormat &format, qint64 audioBufferSize);
-	long long audioLength(const QAudioFormat & format, qint64 microSeconds);
-	void PaintTiles(long long lenght, QByteArray buffer);
 	void paintEvent(QPaintEvent *)override;
-	void PaintTile(int);
-	void smth(int index);
  signals:
 	void LineDrawn(AudioFrame*);
 private slots:
 	void readBuffer();
 	void audioDecoded();
 private:
-	void ReadFormat();
-	int tilePixelOffset(qint64 positionOffset) const;
-	void CalculateTiles();
 	bool isFirstTimeRead = true;
-	void CreatePixmaps(QSize);
 	QScopedPointer<QAudioDecoder> decoder;
 	QAudioFormat format;
-	long tileLenght;
+	long sampleLenght;
 	QScopedPointer<AudioAnalyser> analyser;
-	long windowLenght;
 	AudioRecognition recognizer;
-	long long windowSize;
-	QSize pixmapSize;
-	long long bufferLenght;
 	QString path;
-	long long bufferPosition;
-	QByteArray data;
-	int singlebufferLenght;
-	std::vector<QAudioBuffer> buffers;
-	const long long  windowsDuration = 500 * 1000;
-	std::vector<QPixmap*> pixmaps;
 	Ui::AudioFrame ui;
-
 	// new things
 	std::vector<double> audioFrames;
 	std::vector<double> audioSamples;
